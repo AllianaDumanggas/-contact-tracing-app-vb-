94f55438be9387b2b90085bae2d83f8650020089
unknown <urceline0130@gmail.com>
2022-06-30 23:01:37 +0800

    adding functionality to a button which is to generate info to qr code and save this in the file manager

--- Form4.cs
+++ Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace Contact_Tracing_App
 {
@@ -19,7 +20,18 @@ namespace Contact_Tracing_App
 
         private void generate_qr_Click(object sender, EventArgs e)
         {
+            QRCodeGenerator QR = new QRCodeGenerator();
+            QRCodeData data = QR.CreateQrCode(info.Text, QRCodeGenerator.ECCLevel.H);
+            QRCode qrcode = new QRCode(data);
+            qrpicbox.Image = qrcode.GetGraphic(5);
 
+            string Initialdirectory = @"C:\Users\universal\Pictures";
+            var dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Initialdirectory;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                qrpicbox.Image.Save(dialog.FileName); 
+            }
         }
     }
 }
